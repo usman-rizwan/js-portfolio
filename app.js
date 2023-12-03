b3983cc8e1c29968f392e8c6326ed3c0f1aa999e
usman_rizwan <usmanrizwan771@gmail.com>
2023-12-03 16:58:04 +0500

    time updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let showLoader = () => {
         loader.classList.toggle("hidden");
         content.classList.toggle("hidden");
 
-    }, 2000);
+    }, 3000);
     content.classList.toggle("hidden");
 }
 showLoader()
